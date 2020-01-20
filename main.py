a9857ed4d2f07622656a538a94f1597bb3a1ae3e
Joppe Blondel <joppe@blondel.nl>
2020-01-20 11:34:17 +0100

    Prepared for merge to master
    Signed-off-by: Joppe Blondel <joppe@blondel.nl>

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import random
 import controller.aruco as aruco
 import NetworkEmulator.netemuclient as netemuclient
 
-
 def recv(data:bytes, rssi:int):
     print(rssi, data)
 
@@ -13,27 +12,27 @@ def main():
     # Read server ip from server.ip
     # Connect to network emulator server
     # Receive the maze
-    ip, port = "", 8080
-    with open("server.ip") as f:
-        ip = f.read()
-    network = netemuclient.NetEmuClient(recv, ip, port)
-    network.start()
-    network.waitForMaze()
+    # ip, port = "", 8080
+    # with open("server.ip") as f:
+    #     ip = f.read()
+    # network = netemuclient.NetEmuClient(recv, ip, port)
+    # network.start()
+    # network.waitForMaze()
 
-    x = random.randint(0,15)
-    y = random.randint(0,15)
-    network.position(x, y)
-    network.txpower(0.4)
+    # x = random.randint(0,15)
+    # y = random.randint(0,15)
+    # network.position(x, y)
+    # network.txpower(0.4)
 
     while True:
-        i=input()
-        network.send(bytes(i, 'utf-8'))
+        # i=input()
+        # network.send(bytes(i, 'utf-8'))
         print(aruco.get_result())
 
 if __name__ == "__main__":
     try:
         main()
-        #gopigo.stop()
+        gopigo.stop()
     except KeyboardInterrupt:
         gopigo.stop()
         aruco.stop_pls = True 
