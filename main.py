f3352f77fbbbf45e2131adfd2afbf0141804821c
Baris Imre <b.imre@student.utwente.nl>
2020-01-20 11:01:13 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,27 @@
+import aruco
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+        gopigo.stop()
+    except KeyboardInterrupt:
+        gopigo.stop() 
